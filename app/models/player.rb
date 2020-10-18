20738c3eb0f48e693111ae10341263f478c21498
celtic93 <egorka80@list.ru>
2020-10-18 23:59:34 +0300

    Added achieved? method for Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,13 @@
 class Player < ApplicationRecord
+  LAST_MATCHES_COUNT = 5
+
   has_many :game_achievements
   belongs_to :team
 
   validates :name, presence: true
+
+  def achieved?(achievement)
+    games_ids = Game.order(date: :desc).limit(5).pluck(:id)
+    GameAchievement.exists?(player_id: id, achievement_id: achievement.id, game_id: games_ids)
+  end
 end
