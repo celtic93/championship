5c4f922d9976daecd1c19cd7aee2d394a770bb77
celtic93 <egorka80@list.ru>
2020-10-21 05:39:17 +0300

    Added GameAchievement validation with spec

--- app/models/game_achievement.rb
+++ app/models/game_achievement.rb
@@ -2,4 +2,6 @@ class GameAchievement < ApplicationRecord
   belongs_to :achievement
   belongs_to :game
   belongs_to :player
+
+  validates :player_id, uniqueness: { scope: [:game_id, :achievement_id] }
 end
