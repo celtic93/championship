e12d02993fa11f5ee8a5fef217cbecf14bc33035
celtic93 <egorka80@list.ru>
2020-10-21 04:06:26 +0300

    Added Player played_in method with specs

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,7 @@ class Player < ApplicationRecord
   LAST_MATCHES_COUNT = 5
 
   has_many :game_achievements
+  has_many :games, ->(user) { unscope(where: :player_id).where("home_team_id = ? OR guest_team_id = ?", user.team_id, user.team_id) }
   belongs_to :team
 
   validates :name, presence: true
@@ -9,5 +10,9 @@ class Player < ApplicationRecord
   def achieved?(achievement)
     games_ids = Game.order(date: :desc).limit(LAST_MATCHES_COUNT).pluck(:id)
     GameAchievement.exists?(player_id: id, achievement_id: achievement.id, game_id: games_ids)
+  end
+
+  def played_in?(game)
+    games.include?(game)
   end
 end
